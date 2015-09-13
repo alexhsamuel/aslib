cddcf1e01a1d0ba5a126e25cb9b24de5bdf4ab13
Alex Samuel <alex@alexsamuel.net>
2015-09-13 10:56:48 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+This module contains code of the sort that might be included in the [Python
+standard library](https://docs.python.org/3/library/index.html), but isn't.
+
---
+++ ply/iter/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tools for working with iterators.
+"""
+
+#-------------------------------------------------------------------------------
+
+from   itertools import *
+
+from   .recipes import *
+
