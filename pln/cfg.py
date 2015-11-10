f5315b3d64adfc7804cec0d00d227d0f6ebe1cdb
Alex Samuel <alex@alexsamuel.net>
2015-11-10 17:49:04 -0500

    WIP

--- pln/cfg.py
+++ pln/cfg.py
@@ -8,15 +8,30 @@ def idem(obj):
     return obj
 
 
-class Item:
+class Var:
 
-    def __init__(self, ctor=None, default=None, help=None):
+    def __init__(self, ctor=idem, default=NO_DEFAULT, help=None):
         self.__ctor = ctor
+        self.__default = default
         self.__help = help
 
 
     def __repr__(self):
-        return py.format_ctor(self, self.__ctor, self.__help)
+        kw_args = dict(ctor=self.__ctor, help=self.__help)
+        try:
+            kw_args["default"] = self.__default
+        except AttributeError:
+            pass
+        return py.format_ctor(self, **kw_args)
+
+
+    def __str__(self):
+        result = getattr(self.__ctor, "__name__", str(self.__ctor))
+        if self.__default is not NO_DEFAULT:
+            result += " default={!r}".format(self.__default)
+        if self.__help is not None:
+            result += " : " + help
+        return result
 
 
     @property
@@ -42,116 +57,121 @@ class Item:
 
 
 
-def _check_key(key):
-    if key.isidentifier():
-        return key
+def _check_name(name):
+    if name.isidentifier():
+        return name
     else:
-        raise ValueError("invalid key '{}'".format(key))
+        raise ValueError("invalid name '{}'".format(name))
 
 
 class Group:
 
-    def __init__(self, items):
-        self.__items = items
+    def __init__(self, args={}, **kw_args):
+        self.vars = {}
+        self.update(args, **kw_args)
 
 
     def __repr__(self):
-        return py.format_ctor(self, self.__items)
+        return py.format_ctor(self, self.vars)
 
 
-    def __setitem__(self, key, item):
-        assert isinstance(item, (Item, Group))
-        self.__items[__check_key(key)] = item
+    def __setitem__(self, name, value):
+        name = _check_name(name)
+
+        if isinstance(value, (Group, Var)):
+            self.vars[name] = value
+        elif callable(value):
+            self.vars[name] = Var(ctor=value)
+        else:
+            self.vars[name] = Var(ctor=type(value), default=value)
 
 
-    def __getitem__(self, key):
-        key = _check_key(key)
+    def __getitem__(self, name):
+        name = _check_name(name)
         try:
-            return self.__items[key]
+            return self.vars[name]
         except KeyError:
-            raise KeyError(key) from None
+            raise KeyError(name) from None
 
 
     def update(self, args={}, **kw_args):
-        for key, item in args.items():
-            self.__setitem__(key, item)
-        for key, item in kw_args.items():
-            self.__setitem__(key, item)
-
-
-
-class item:
-
-    def __init__(self, default=NO_DEFAULT, ctor=idem, help=None):
-        self.default = default
-        self.ctor = ctor
-        self.help = help
-
-
-
-def group(args={}, **kw_args):
-    items = {}
-    vals = {}
-    for name, var in chain(args.items(), kw_args.items()):
-        if isinstance(var, item):
-            items[name] = Item(ctor=var.ctor, help=var.help)
-            if var.default is not NO_DEFAULT:
-                vals[name] = var.default
-        elif isinstance(var, Cfg):
-            items[name] = Cfg(var._group, dict(var._vals))
-        elif callable(var):
-            items[name] = Item(ctor=var)
-        else:
-            items[name] = Item(ctor=type(var))
-            vals[name] = var
-    return Cfg(Group(items), vals)
+        for name, var in args.items():
+            self.__setitem__(name, var)
+        for name, var in kw_args.items():
+            self.__setitem__(name, var)
 
 
 
 class Cfg:
 
-    def __init__(self, group, vals={}):
+    def __init__(self, group, args={}):
+        vals = {}
+        for name, var in group.vars.items():
+            if isinstance(var, Group):
+                vals[name] = Cfg(var, args.pop(name, {}))
+            else:
+                try:
+                    val = args.pop(name)
+                except KeyError:
+                    pass
+                else:
+                    vals[name] = val
+
+        if len(args) > 0:
+            raise LookupError("no vars " + ", ".join(args.keys()))
+
         object.__setattr__(self, "_Cfg__group", group)
-        object.__setattr__(self, "_Cfg__values", vals)
+        object.__setattr__(self, "_Cfg__vals", vals)
 
 
     def __repr__(self):
-        return py.format_ctor(self, self.__group, self.__values)
+        return py.format_ctor(self, self.__group, self.__vals)
 
 
-    @property
-    def _group(self):
-        return self.__group
+    def __str__(self):
+        return "\n".join(self.__format()) + "\n"
 
 
-    @property
-    def _vals(self):
-        return self.__values
+    def __format(self, indent="  "):
+        for name, var in sorted(self.__group.vars.items()):
+            if isinstance(var, Group):
+                cfg = getattr(self, name)
+                yield "{}.".format(name)
+                for line in cfg.__format(indent):
+                    yield indent + line
+            else:
+                try:
+                    val = self.__vals[name]
+                except KeyError:
+                    val = ""
+                else:
+                    val = "={!r}".format(val)
+                yield "{}{} {}".format(name, val, var)
 
 
     def __setattr__(self, name, value):
         try:
-            item = self.__group[name]
+            var = self.__group[name]
         except KeyError:
             raise AttributeError(name) from None
 
-        if isinstance(item, Group):
+        if isinstance(var, Group):
             try:
-                cfg = self.__values[name]
+                cfg = self.__vals[name]
             except KeyError:
-                cfg = self.__values[name] = Cfg(item)
+                cfg = self.__vals[name] = Cfg(var)
             cfg(value)
 
         else:  
-            self.__values = item.convert(value)
+            self.__vals[name] = var.convert(value)
 
 
     def __getattr__(self, name):
         try:
-            return self.__values[name]
+            return self.__vals[name]
         except KeyError:
             try:
-                self.__group[name]
+                var = self.__group[name]
             except KeyError:
                 raise AttributeError("no config " + name) from None
             else:
@@ -159,7 +179,7 @@ class Cfg:
 
 
     def __call__(self, args={}, **kw_args):
-        for name, value in args.item():
+        for name, value in args.items():
             self.__setattr__(name, value)
         for name, value in kw_args.items():
             self.__setattr__(name, value)
@@ -167,10 +187,10 @@ class Cfg:
 
 
 
-DEFAULT_CFG = group(
-    bottom=group(
+DEFAULT_CFG = Group(
+    bottom=Group(
         line                    ="-",
-        separator=group(
+        separator=Group(
             between             =" ",
             end                 ="",
             index               ="  ",
@@ -178,7 +198,7 @@ DEFAULT_CFG = group(
         ),
         show                    =False
     ),
-    float=group(
+    float=Group(
         inf                     ="\u221e",
         max_precision           =8,
         min_precision           =1,
@@ -188,18 +208,23 @@ DEFAULT_CFG = group(
 
 
 print(DEFAULT_CFG)
+print()
+print(Cfg(DEFAULT_CFG))
+print()
 
 
-c = DEFAULT_CFG
+c = Cfg(DEFAULT_CFG)
 c.bottom(
     line                        = "\u2500",
     separator = dict(
-        bottom                  = "\u2500\u2534\u2500",
+        between                 = "\u2500\u2534\u2500",
         end                     = "\u2500\u2518",
         index                   = "\u2500\u2534\u2500",
         start                   = "\u2514\u2500",
     )
 )
+print(c)
+print()
 
 print(repr(c.bottom.separator))
 print(repr(c.bottom.separator.index))
