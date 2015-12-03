962260fd01fcb51fff273a461a121ca7f3a3f04c
Alex Samuel <alex@alexsamuel.net>
2015-12-03 17:59:02 -0500

    HTML-like ANSI terminal markup.

--- pln/terminal/ansi.py
+++ pln/terminal/ansi.py
@@ -6,6 +6,9 @@ See https://en.wikipedia.org/wiki/ANSI_escape_code.
 
 #-------------------------------------------------------------------------------
 
+from   enum import Enum
+import html.parser
+from   html.parser import HTMLParseError as ParseError
 from   math import floor
 
 #-------------------------------------------------------------------------------
@@ -76,15 +79,80 @@ def COLORMAP_BACKGROUND(i):
     return SGR(48, 5, i)
 
 
-BLACK   =   0
-RED     =   1
-GREEN   =   2
-YELLOW  =   3
-BLUE    =   4
-MAGENTA =   5
-CYAN    =   6
-GRAY    =   7
-WHITE   = 231
+#-------------------------------------------------------------------------------
+
+COLOR_NAMES = dict(
+    black       =232,
+    dark_red    =  1,
+    dark_green  =  2,
+    brown       =  3,
+    dark_blue   =  4,
+    purple      =  5,
+    turquoise   =  6,
+    light_gray  =  7,
+    dark_gray   =  8,
+    red         =  9,
+    green       = 10,
+    yellow      = 11,
+    blue        = 12,
+    pink        = 13,
+    cyan        = 14,
+    white       =255,
+)
+
+
+def parse_rgb_triple(triple):
+    if not triple.startswith("#"):
+        raise ValueError("RGB triple doesn't start with #")
+    if len(triple) == 4:
+        return tuple( int(x, 16) * 17 for x in triple[1 : 4] )
+    elif len(triple) == 7:
+        rgb = triple[1 : 3], triple[3 : 5], triple[5 : 7]
+        return tuple( int(x, 16) for x in rgb )
+    else:
+        raise ValueError("wrong number of digits for RGB triple")
+
+
+def get_color(value):
+    """
+    Translates `value` to a color code.
+
+    `value` may be:
+
+    - An integer color code between 0 and 255.
+    - A web-style "#XYZ" or "#XXYYZZ" RGB triple
+    - A grayscale name "grayX" where X is between 0 and 100.
+    - A color name for the sixteen standard colors.
+    """
+    if isinstance(value, int):
+        if 0 <= value < 256:
+            return value
+        else:
+            raise ValueError("color value not between 0 and 255")
+
+    val = str(value)
+
+    if val.startswith("#"):
+        r, g, b = parse_rgb_triple(val)
+        # Convert RGB value range from 0-255 to 0-5.
+        return 16 + 36 * (r * 6 // 256) + 6 * (g * 6 // 256) + (b * 6 // 256)
+
+    if val.startswith("gray"):
+        try:
+            gray_value = int(val[4 :])
+        except ValueError:
+            pass
+        else:
+            if 0 <= gray_value <= 100:
+                return 232 + gray_value * 24 // 101
+
+    try:
+        return COLOR_NAMES[val]
+    except KeyError:
+        pass
+
+    raise ValueError("unrecognized color: {!r}".format(value))
+    
 
 
 def GRAY_LEVEL(fraction):
@@ -93,49 +161,78 @@ def GRAY_LEVEL(fraction):
     """
     assert 0 <= fraction <= 1
     index = int(floor(fraction * 24.999999999999))
-    return WHITE if index == 24 else 232 + index
+    return 231 if index == 24 else 232 + index
 
 
-def sgr(*, color=None, background=None, light=False, bold=False, 
-        underline=False, blink=False, reverse=False, conceal=False):
+def sgr(*, fg=None, bg=None, bold=None, underline=None, blink=None,
+        reverse=None, conceal=None):
     """
-    Returns an SGR sequence.
+    Returns an SGR sequence to set color and text style.
+
+    An argument value of `None` indicates that style should not be modified.
+
+    @param fg
+      Foreground color name or number, or `"default"` for the implementation's
+      default. 
+    @param bg
+      Background color name or number, or `"default"` for the implementation's
+      default. 
+    @param intensity
+      `"bold"`, `"faint"`, or `"normal"`.
+    @param underline
+      True to enable single underlining; false to disable.
+    @param blink
+      True to enable blinking text; false to disable.
+    @param reverse
+      True to enable reverse video; false to disable.
+    @param conceal
+      True to conceal text; false to reveal.
     """
     codes = []
-    if color is not None:
-        codes.extend([38, 5, int(color)])
-    if background is not None:
-        try:
-            codes.append(40 + _COLOR_NAMES[background])
-        except KeyError:
-            codes.extend([48, 5, int(background)])
-    if bold:
-        codes.append(1)
-    if light:
-        codes.append(2)
-    if underline:
-        codes.append(4)
-    if blink:
-        codes.append(5)
-    if reverse:
-        codes.append(7)
-    if conceal:
-        codes.append(8)
+
+    # Avoid using codes 30-37 for foreground and 40-47 for background color,
+    # since the bold setting may change the colors of these.
+
+    if fg is None:
+        pass
+    elif fg == "default":
+        codes.append(39)
+    else:
+        codes.extend((38, 5, get_color(fg)))
+
+    if bg is None:
+        pass
+    elif bg == "default":
+        codes.append(49)
+    else:
+        codes.extend((48, 5, get_color(bg)))
+
+    if bold is not None:
+        codes.append(1 if bold else 21)
+    if underline is not None:
+        codes.append(4 if underline else 24)
+    if blink is not None:
+        codes.append(5 if blink else 25)
+    if reverse is not None:
+        codes.append(7 if reverse else 27)
+    if conceal is not None:
+        codes.append(8 if conceal else 28)
+
     return SGR(*codes)
 
 
-def inverse_sgr(*, color=None, background=None, light=False, bold=False,
-                underline=False, blink=False, reverse=False, conceal=False):
+def inverse_sgr(*, fg=None, bg=None, bold=False, underline=False, blink=False,
+                reverse=False, conceal=False):
     """
     Returns the inverse SGR sequence to `sgr()`.
     """
     codes = []
-    if color is not None:
+    if fg is not None:
         codes.append(39)
-    if background is not None:
+    if bg is not None:
         codes.append(49)
-    if bold or light:
-        codes.append(22)
+    if bold:
+        codes.append(21)
     if underline:
         codes.append(24)
     if blink:
@@ -157,5 +254,112 @@ def style(**kw_args):
     escape = sgr(**kw_args)
     unescape = inverse_sgr(**kw_args)
     return lambda text: escape + str(text) + unescape
+
+
+#-------------------------------------------------------------------------------
+
+class Parser(html.parser.HTMLParser):
+    """
+    Parses pseudo-HTML markup into text with ANSI escapes.
+
+    Syntax:
+
+    - `<foreground color="COLOR"> ... </foreground>` for the foreground color.
+    - `<background color="COLOR"> ... </background>` for the background color.
+    - `<bold> ... </bold>` for bold.
+    - `<underline> ... </underline>`.
+    - `<blink> ... </blink>` for blink.
+    - `<reverse> ... </reverse>` for reverse.
+    - `<conceal> ... </conceal>` for conceal.
+
+    The aliases `<fg>`, `<bg>`, `<b>`, `<u>` may be used for the first four
+    tags above, respectively.
+
+    `COLOR` may be a color name, RGB triplet, or gray value like "gray50".
+
+    Unrecognized tags are passed through.
+
+    """
+
+    # Renaming tags to SGR attributes.
+    __RENAME = {
+        "b": "bold",
+        "background": "bg",
+        "foreground": "fg",
+        "u": "underline",
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.__result = []
+        # Stack of tags, for matching end tags.
+        self.__tags = []
+        # For colors, stacks of nested values.
+        self.__colors = dict(fg=[], bg=[])
+        # For boolean attributes, a nesting count.
+        self.__attrs = dict(bold=0, underline=0, blink=0, reverse=0, conceal=0)
+
+
+    def handle_starttag(self, tag, attrs):
+        tag = self.__RENAME.get(tag, tag)
+        self.__tags.append(tag)
+        
+        if tag in self.__colors:
+            if len(attrs) != 1 or attrs[0][0] != "color":
+                raise ParseError("{} must have attribute color".format(tag))
+            color = get_color(attrs[0][1])
+            stack = self.__colors[tag]
+            # Set the color.
+            if len(stack) == 0 or stack[-1] != color:
+                self.__result.append(sgr(**{tag: color}))
+            # Push it.
+            stack.append(color)
+
+        elif tag in self.__attrs:
+            # Set the attribute, if this is the outermost tag.
+            if self.__attrs[tag] == 0:
+                self.__result.append(sgr(**{tag: True}))
+            # Increment the nesting count.
+            self.__attrs[tag] += 1
+
+        else:
+            self.__result.append(self.get_starttag_text())
+
+
+    def handle_endtag(self, tag):
+        tag = self.__RENAME.get(tag, tag)
+        if len(self.__tags) == 0 or tag != self.__tags.pop():
+            raise ParseError("mismatched end tag {!r}".format(tag))
+
+        if tag in self.__colors:
+            stack = self.__colors[tag]
+            color = stack.pop()
+            old_color = "default" if len(stack) == 0 else stack[-1]
+            # Restore the previous color.
+            if old_color != color:
+                self.__result.append(sgr(**{tag: old_color}))
+
+        elif tag in self.__attrs:
+            self.__attrs[tag] -= 1
+            # Turn off the attribute, if this is the outermost tag.
+            if self.__attrs[tag] == 0:
+                self.__result.append(sgr(**{tag: False}))
+
+        else:
+            self.__result.append("</" + tag + ">")
+
+
+    def handle_data(self, data):
+        # Just append non-tag text.
+        self.__result.append(data)
+
+
+    @property
+    def result(self):
+        """
+        The parsed and converted result.
+        """
+        return "".join(self.__result)
+
 
 
