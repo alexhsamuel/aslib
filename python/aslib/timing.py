f5d4f3b66e8bd344769b46e10e2c6540a67c22c1
Alex Samuel <alex@alexsamuel.net>
2016-08-31 17:41:36 +0200

    New, smarter timing.

--- python/aslib/timing.py
+++ python/aslib/timing.py
@@ -132,67 +132,55 @@ MSEC = 1e-3
 USEC = 1e-6
 NSEC = 1e-6
 
-def _time(fn, count):
+def _time1(fn, args, kw_args):
+    start = perf_counter()
+    fn(*args, **kw_args)
+    return perf_counter() - start
+
+
+def _time(count, fn, args, kw_args):
+    if count == 1:
+        return _time1(fn, args, kw_args)
+
     start = perf_counter()
     for _ in range(count):
-        fn()
-    return (perf_counter() - start) / count
+        fn(*args, **kw_args)
+    return perf_counter() - start
 
 
-def _estimate_time(fn, *, min_time=USEC):
+def _estimate_count(min_time, fn, args, kw_args):
     """
-    Estimates the time to invoke `fn()`.
+    Estimates the time to invoke `fn(*args, **kw_args)`.
     """
-    # Number of iterations to time.  
     count = 1
-    while True:
-        # Time 'count' iterations.
-        elapsed = _time(fn, count)
-        # Is the total time significant?
-        if elapsed > min_time:
-            return elapsed / count
-        else:
-            # No.  Try again with more iterations.
-            count *= 10
+    elapsed = _time1(fn, args, kw_args)
+
+    while elapsed < min_time:
+        count *= 10
+        elapsed = _time(count, fn, args, kw_args)
+
+    return count
 
 
-def _time_it(fn, *, samples=10, count=1):
+def call_timer(samples=100, *, warm_up=1, min_sample_time=10 * USEC, quantile=0.1):
     """
-    Times callable `fn`.
-
-    @param samples
-      Number of samples.
-    @param count
-      Number of invocations per trial, or `None` to choose automatically
-      based on `target`.
-    @param target
-      Approximate total time in seconds for all samples, used to choose `count` 
-      automatically.
-    @param warm_up
-      Number of times to invoke `fn` as a warm-up before timing.
-    @return
-      Generator of trial times.  Each time is the total time for the trial
-      divided by the number of invocations per trial.
-    """
-    return ( _time(fn, count) for _ in range(samples) )
-
-    
-def timer(samples=100, warm_up=1, min_sample_time=10 * USEC):
-    """
-    Creates a timer.
+    Creates a function call timer.
     """
     def time(fn, *args, **kw_args):
-        call = lambda: fn(*args, **kw_args)
-
         # Do some un-timed warm up calls.
         for _ in range(warm_up):
-            call()
+            fn(*args, **kw_args)
 
-        time_estimate = _estimate_time(call)
-        count = max(1, round(min_sample_time / time_estimate))
+        # Choose the count so the sample time isn't too small.
+        count = _estimate_count(min_sample_time, fn, args, kw_args)
 
-        times = sorted(_time_it(call, samples=samples, count=count))
-        time = times[len(times) // 10]
+        # Sample the timing.
+        times = sorted( 
+            _time(count, fn, args, kw_args) / count 
+            for _ in range(samples) )
+
+        # Use the given quantile as the timing result.
+        time = times[int(len(times) * quantile)]
 
         return {
             "name"      : fn.__name__,
