977125176c86977bcd2bf7632035d34736321753
Alex Samuel <alex@alexsamuel.net>
2016-09-05 11:48:32 +0100

    Add memoize_method().

--- python/aslib/memo.py
+++ python/aslib/memo.py
@@ -39,3 +39,38 @@ def memoize(fn):
     return memoize_with({})(fn)
 
 
+def memoize_method(fn):
+    """
+    Memoizes an ordinary method.
+
+    Uses a memo dictionary attached as an attribute to each instance of the
+    containing class.  The attribute name is stored in `__memo_name__` on the
+    method function.
+
+    ```py
+    class C:
+
+        @memoize_method
+        def expensive(self, arg):
+            # ...
+            return result
+
+    ```
+    """
+    name = "__" + fn.__name__
+    @functools.wraps(fn)
+    def memoized(self, *args, **kw_args):
+        # FIXME: Bind to signature first.
+        key = args + tuple(sorted(kw_args.items()))
+        memo = self.__dict__.setdefault(name, {})
+        try:
+            return memo[key]
+        except KeyError:
+            value = memo[key] = fn(self, *args, **kw_args)
+            return value
+
+    memoized.__memo_name__ = name
+    return memoized
+
+
+
